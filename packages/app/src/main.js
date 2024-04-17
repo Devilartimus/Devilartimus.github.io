fa5315ab6b376394d4e28a05fa4d3f84ffc96d44
Devilartimus <devilartimus6@gmail.com>
2024-04-18 02:55:12 +0700

    fffff

--- packages/app/src/main.js
+++ packages/app/src/main.js
@@ -21,8 +21,8 @@ import i18n from './lang';
 import App from './App.vue';
 
 // Обновленные пути к CSS файлам
-import '././prosemirror.css';  // Путь к файлу prosemirror.css изменен на относительный
-import '././tables.css';        // Теперь tables.css находится в папке src
+import './packages/app/prosemirror.css';  // Путь к файлу prosemirror.css изменен на относительный
+import './packages/app/tables.css';        // Теперь tables.css находится в папке src
 
 import 'virtual:windi.css';
 import 'floating-vue/dist/style.css';
